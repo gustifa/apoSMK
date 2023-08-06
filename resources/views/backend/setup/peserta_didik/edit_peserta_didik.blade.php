9bfbcbd5186d2edf8730def117e2b9a9b2b07b74
gatech <fauzangustifa@gmail.com>
2023-08-06 15:53:36 +0700

    Update Backend

--- resources/views/backend/setup/peserta_didik/edit_peserta_didik.blade.php
+++ resources/views/backend/setup/peserta_didik/edit_peserta_didik.blade.php
@@ -3,7 +3,7 @@
 
 <!--start page wrapper -->
 <!-- <script src="{{asset('admin/assets/download/jquery.min.js')}}"></script> -->
-	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<script src="{{asset('admin/assets/download/jquery-3.5.1.js')}}"></script>
 <div class="page-wrapper">
 	<div class="page-content">
 		
