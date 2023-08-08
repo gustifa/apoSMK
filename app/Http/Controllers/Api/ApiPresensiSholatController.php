ef3a527525fb581ee9a53bf696342b287f9ff128
gatech <fauzangustifa@gmail.com>
2023-08-08 12:03:39 +0700

    modified:   app/Http/Controllers/Api/ApiPresensiSholatController.php

--- app/Http/Controllers/Api/ApiPresensiSholatController.php
+++ app/Http/Controllers/Api/ApiPresensiSholatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PresensiSholat;
+use App\Models\WaktuSholat;
 class ApiPresensiSholatController extends Controller
 {
      public function lihatPresensiSholat(){
@@ -15,7 +16,13 @@ class ApiPresensiSholatController extends Controller
     }
 
     public function simpanPresensiSholat(Request $request){
-        $data = new PresensiSholat();
+        $time = strtotime(date('H:i'));
+        $waktuZuhurMulai = (WaktuSholat::where('nama', 'Zhuhur')->select('waktu_mulai', 'waktu_selesai')->get())->implode('waktu_mulai');
+        $waktuZuhurSelesai = (WaktuSholat::where('nama', 'Zhuhur')->select('waktu_mulai', 'waktu_selesai')->get())->implode('waktu_selesai');
+        $selectedTimeZuhur = strtotime(date($waktuZuhurMulai));
+        $endTimeZuhur = strtotime(date($waktuZuhurSelesai));
+        if($time >= $waktuZuhurMulai && $time <= $waktuZuhurSelesai){
+          $data = new PresensiSholat();
         $data->rfid_id = $request->rfid_id;
         $data->presensi = $request->presensi;
         // $data->created_at = Carbon::now();
@@ -23,6 +30,13 @@ class ApiPresensiSholatController extends Controller
 
         return response()->json([
             "message" => "Presensi Sholat Berhasil ditambahkan"
+        ], 201);  
+    }else{
+        return response()->json([
+            "message" => "Tidak Jadwal Sholat"
         ], 201);
+    }
+       
+        
     }
 }
